44c21944bdbb04f97efecc70ee2d885464ba361e
Your Name <you@example.com>
2022-05-21 17:48:52 +0530

    mobile started

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ window.addEventListener("scroll", function() {
     mainH.classList.toggle("sticky", window.scrollY > mainH.scrollHeight);
     
   });
+   
+//  Header styling for display the home page
+
+function displayMenu(){
+	document.getElementById("homeNav").style.display ="flex";
+}
+
+document.getElementById("displayMenuBar").addEventListener("click", displayMenu);
