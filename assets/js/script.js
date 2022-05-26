6176d7f831e12f74928536802754ee0d8e8106b0
Your Name <you@example.com>
2022-05-26 16:17:51 +0530

    mobile view trypage started

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,46 @@ window.addEventListener("scroll", function() {
 
 function displayMenu(){
 	document.getElementById("homeNav").style.display ="flex";
+ 
 }
 function hideMenu(){
   document.getElementById("homeNav").style.display ="none";
+  
 }
 
 document.getElementById("displayMenuBar").addEventListener("click", displayMenu);
 document.getElementById("closingIcon").addEventListener("click", hideMenu);
+
+
+
+
+// Home and Company button clicking changes
+
+function companyButtnDisplay(){
+   var selectedCompText = document.getElementById("companyTextMobile");
+   var selectedHomeText = document.getElementById("homeTextMobile");
+   var selectedCompButton = document.getElementById("companyButtonMobile");
+   var selectedHomeButton = document.getElementById("homeButtonMobile");
+   selectedCompText.classList.remove("cm-text-hs");
+   selectedHomeText.classList.add("cm-text-hs");
+   selectedCompButton.classList.remove("cm-icon");
+   selectedCompButton.classList.add("cm-text");
+   selectedHomeButton.classList.remove("cm-text");
+   selectedHomeButton.classList.add("cm-icon");
+}
+
+function HomeButtnDisplay(){
+   var selectedCompText = document.getElementById("companyTextMobile");
+   var selectedHomeText = document.getElementById("homeTextMobile");
+   var selectedCompButton = document.getElementById("companyButtonMobile");
+   var selectedHomeButton = document.getElementById("homeButtonMobile");
+   selectedHomeText.classList.remove("cm-text-hs");
+   selectedCompText.classList.add("cm-text-hs");
+   selectedHomeButton.classList.remove("cm-icon");
+   selectedHomeButton.classList.add("cm-text");
+   selectedCompButton.classList.remove("cm-text");
+   selectedCompButton.classList.add("cm-icon");
+}
+
+document.getElementById("companyButtonMobile").addEventListener("click", companyButtnDisplay);
+document.getElementById("homeButtonMobile").addEventListener("click", HomeButtnDisplay);
